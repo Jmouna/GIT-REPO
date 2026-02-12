313054300933e40d4395ec132abc73e394057890
Jmouna-ISLAIB <jarraya.mouna@islaib.u-jendouba.tn>
2026-02-12 22:05:49 +0100

    Patch code

--- src/application/Test.java
+++ src/application/Test.java
@@ -1,22 +1,13 @@
 package application;
-
+//class Test.java
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
 		// Commentaire HELLO
-		System.out.println("Je suis la branche master");
-=======
-		// Salut
-		//Un autre salut
-		System.out.println("Hello je suis la branche dev");
-<<<<<<< HEAD
->>>>>>> dev
-=======
-		System.out.println("merge");
-		System.out.println("dernier essai");
->>>>>>> dev
+		String msg="Hello"
+		System.out.println(msg);
+
 	}
 
 }

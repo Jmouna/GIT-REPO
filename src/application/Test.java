c6af00419ab4ade82d7dd72169b5293b6193303e
Jmouna-ISLAIB <jarraya.mouna@islaib.u-jendouba.tn>
2026-03-03 12:50:16 +0100

    dernier commit

--- src/application/Test.java
+++ src/application/Test.java
@@ -4,19 +4,10 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
+
 		// Commentaire HELLO
-		System.out.println("Je suis la branche master");
-=======
-		// Salut
-		//Un autre salut
-		System.out.println("Hello je suis la branche dev");
-<<<<<<< HEAD
->>>>>>> dev
-=======
-		System.out.println("merge");
-		System.out.println("dernier essai");
->>>>>>> dev
+		System.out.println("Hello");
+
 	}
 
 }
